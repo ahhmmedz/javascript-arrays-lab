37769b4b6307ae7e0e4731f028b708996449a0a0
Ahmed <ahmedmohdqamber@gmail.com>
2026-07-22 20:22:34 +0300

    finished until exercise 11

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
 console.log('Js Arrays Lab!')
+
+// Exercise 1: 
+
+const foods = []
+
+console.log(`Exercise 1 result:`, foods)
+
+// Exercise 2:
+
+foods.push('pizza','cheeseburger')
+console.log('Exercise 2 result:', foods)
+
+// Exercise 3:
+
+foods.unshift('taco')
+console.log(foods)
+
+// Exercise 4:
+
+const favFood = foods[1]
+console.log('Exercise 4 result:' ,favFood)
+
+// Exercise 5:
+
+foods.splice(2, 0, 'tofu')
+console.log("Exercise 5 result:", foods)
+
+// Exercise 6: 
+
+foods.splice(1,1,'sushi','cupcake')
+console.log("Exercise 6 result:", foods)
+
+// Exercise 7:
+
+const yummy = foods.slice(1, 3);
+console.log('Exercise 7 result:', yummy);
+
+// Exercise 8:
+
+const soyIdx = foods.indexOf('tofu') 
+console.log('Exercise 8 result:', soyIdx);
+
+// Exercise 9: 
+
+const allFoods = foods.join('-->')
+console.log('Exercise 9 result:', allFoods)
+
+// Exercise 10:
+
+const hasSoup = foods.includes('soup')
+console.log('Exercise 10 result:', hasSoup)
